3c059e37b5cdf636bde3f17ac3dc28a8ecf1f97c
Will Saunders <will@cia-adv.com>
2018-10-09 18:02:13 -0700

    more content

--- web/index.php
+++ web/index.php
@@ -10,13 +10,71 @@
 
 </head>
 <body>
-	<header>
-		<div class="logo"><a href="/"></a></div>
-		<div class="awards text">AWARDS 2018</div>
-		<div class="fyc text">FOR YOUR CONSIDERATION</div>
+	<header class="">
+		<div class="flipper">
+			<div class="front">
+				<div class="logo"><a href="/"></a></div>
+				<div class="awards text">AWARDS 2018</div>
+				<div class="fyc text">FOR YOUR CONSIDERATION</div>
+			</div>
+			<div class="back">
+				<div class="logo"><a href="/"></a></div>
+				<div class="awards text">AWARDS 2018</div>
+				<div class="fyc text">FOR YOUR CONSIDERATION</div>
+			</div>
+		</div>
 	</header>
+	<div class="content">
+		<div class="home page">
+			<h3>BEST ANIMATED SHORT</h3>
+			<div id="bird-karma" class="poster">
+				<img class="tall" src="img/bird-karma_poster.png">
+			</div>
+			<div id="bilby" class="poster">
+				<img class="tall" src="img/bilby_poster.png">
+			</div>
+		</div>
+		<div class="bilby page">
+			<div class="logo"></div>
+			<div class="left col">
+				
+				<div class="menu">
+					<ul class="nav">
+						<li class="current">FILMMAKERS
+							<ul class="sub-nav">
+								<li class="current">KELLY COONEY</li>
+								<li>PIERRE PERIFEL</li>
+								<li>JP SANS</li>
+								<li>LIRON TOPAZ</li>
+							</ul>
+						</li>
+						<li>PHOTOS</li>
+						<li>VIDEOS</li>
+					</ul>
+				</div>
+			</div>
+			<div class="right col">
+				<div class="filmmakers">
+					<div>
+						<div class="maker-photo"><img src="img/bilby/Cooney-bio-pic.jpg"></div>
+						<div class="bio">
+							
+							<h4>KELLY COONEY, PRODUCER</h4>
+							<p>Kelly Cooney is a Producer on the Dreamworks<br/>Animation short “Bilby.” She also currently serves as a<br/>Co-Producer on the upcoming Dreamworks Animation<br/>release “Trolls World Tour.” </p>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<script src="/js/scripts.js"></script>
+							<p>Her other credits include Production Manager on<br/>“Trolls,” “Puss in Boots,” “Kung Fu Panda: Secrets of the<br/>Scroll,” and the Netflix series “Dragons: Riders of Berk.”<br/>In addition to working in the story department on <br/>“Shrek 2” and “Shrek the Third,” Kelly also spent several<br/>years working as a creative executive in development for<br/>DreamWorks Animation. </p>
+
+							<p>Kelly is originally from Minnesota and attended the<br/>University of Notre Dame, where she studied Film,<br/>Television, and Theater.</p>
+							
+						</div>
+					</div>
+				</div>
+				<div class="photos"></div>
+				<div class="videos"></div>
+			</div>
+		</div>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="/js/scripts.js"></script>
 </body>
 </html>
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+	$('.poster').on('click', function(){
+		var id = $(this).attr('id');
+		$('header, .content').addClass('flipped');
+		$('.page.'+id).delay(500).fadeIn();
+	})
 });
